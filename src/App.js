1c174e49d51d8c125b1d9f11fd3fd70020be7a7a
Chris Mckeown <chris.j.mckeown@hotmail.com>
2021-06-30 09:20:00 +1000

    header added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const HatsPage = () => (
   <div>
@@ -33,6 +34,7 @@ const Mens = () => (
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
